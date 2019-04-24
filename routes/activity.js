9450ad352623dce32df31c167c45ba5b97337c65
srikantkota <49935511+srikantkota@users.noreply.github.com>
2019-04-24 16:04:52 +0800

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -63,7 +63,8 @@ exports.edit = function (req, res) {
  */
 exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
+    console.log( req.body );
+    console.log("save");
     logData(req);
     res.send(200, 'Save');
 };
